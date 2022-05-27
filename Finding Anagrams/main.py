1ed9be272b4edfbb73d1d770e824b71618999e3f
Julius Odunuga <40062040+Julisam@users.noreply.github.com>
2022-05-27 09:09:50 +0100

    Create main.py

---
+++ Finding Anagrams/main.py
@@ -0,0 +1,10 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagrams(word, anagram):
+    # [assignment] Add your code here
+
+    return sorted(word.lower()) == sorted(anagram.lower())
